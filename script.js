fe45492efc9b4786419fa0c179f534d2147634f7
wisdommichael-2 <wisdom.opi3@gmail.com>
2026-09-19 09:40:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -167,3 +167,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+// Look for code in script.js that does this:
+document.addEventListener('click', (e) => {
+    const link = e.target.closest('a');
+    if (link) {
+        // IF IT HAS THIS:
+        // window.location.href = ... or e.preventDefault()
+    }
+});
